308a1026b5a7fb1448986563ae0ee37436884a93
Ukani Dhruv <126572525+Dhruv-Ukani95@users.noreply.github.com>
2023-12-30 15:54:00 +0530

    Add files via upload

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -1,6 +1,7 @@
 const  User  = require("../models/User");
 const bcrypt = require('bcryptjs');
 const Booking = require("../models/Booking");
+const multer = require('multer');
 
 const getAllUser = async (req,res,next) => {
     let users;
@@ -126,4 +127,17 @@ const getUserById = async (req, res, next) => {
     }
     return res.status(200).json({users})
 }
-module.exports = {getAllUser, signup, updateUser, deleteUser, login, getBookingsofUser, getUserById};
+
+const imageUpload = multer({
+    storage: multer.diskStorage({
+        destination: function(req, file, cb)
+        {
+            cb(null,'uploads');
+        },
+        filename: function(req, file, cb)
+        {
+            cb(null, file.fieldname+ "-"+Date.now()+".jpg")
+        }
+    })
+}).single("profile_image")
+module.exports = {getAllUser, signup, updateUser, deleteUser, login, getBookingsofUser, getUserById, imageUpload};
